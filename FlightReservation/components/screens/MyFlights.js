e425d9fc83c8c363079802ab2511d9493a4b61f6
Alonso-AS <alfredoalonso_alcaraz@ucol.mx>
2021-04-13 11:59:46 -0500

    FlatList Component Implementation
    The FlatList that will contain the flights saved in the Firebase
    database has been implemented.
    
    Co-authored-by: YilKanda <yilkanda@hotmail.com>

--- FlightReservation/components/screens/MyFlights.js
+++ FlightReservation/components/screens/MyFlights.js
@@ -1,51 +1,20 @@
 import React from 'react';
-import {View, ScrollView, Text} from 'react-native';
+import {View, FlatList, Text} from 'react-native';
 import CrossPlatformIcon from 'react-native-cross-platform-icons';
 import MyFlightsStyles from '../assets/styles/MyFlightsStyles';
+import ItemsMyFlights from './ItemsMyFlights';
 
+const DATA = [{id: 1}];
 const MyFlights = () => {
   return (
     <View style={[MyFlightsStyles.container, MyFlightsStyles.centerVertical]}>
       <Text style={MyFlightsStyles.lblTitle}>My flights</Text>
-      <ScrollView style={MyFlightsStyles.marginTitle}>
-        <View>
-          <View style={[MyFlightsStyles.row, MyFlightsStyles.spaceBetween]}>
-            <View style={MyFlightsStyles.column}>
-              <Text style={MyFlightsStyles.fontBold}>BEG</Text>
-              <Text style={MyFlightsStyles.fontNormal}>Serbia</Text>
-            </View>
-            <View
-              style={[MyFlightsStyles.column, MyFlightsStyles.centerVertical]}>
-              <CrossPlatformIcon
-                name="airplane"
-                size={25}
-                styles={MyFlightsStyles.centerHorizontal}
-                color="rgba(74,144,226,1)"
-                outline
-              />
-            </View>
-            <View style={MyFlightsStyles.column}>
-              <Text
-                style={[MyFlightsStyles.textRight, MyFlightsStyles.fontBold]}>
-                AMS
-              </Text>
-              <Text
-                style={[MyFlightsStyles.textRight, MyFlightsStyles.fontNormal]}>
-                Netherlands
-              </Text>
-            </View>
-          </View>
-          <View style={MyFlightsStyles.hr} />
-          <View style={[MyFlightsStyles.row, MyFlightsStyles.spaceBetween]}>
-            <Text style={MyFlightsStyles.fontNormal}>September 3, 2020</Text>
-            <Text
-              style={[MyFlightsStyles.textRight, MyFlightsStyles.fontNormal]}>
-              2 passengers
-            </Text>
-          </View>
-          <View style={MyFlightsStyles.hr} />
-        </View>
-      </ScrollView>
+      <FlatList
+        style={MyFlightsStyles.marginTitle}
+        data={DATA}
+        renderItem={({item}) => <ItemsMyFlights item={item} />}
+        keyExtractor={item => item.id}
+      />
       <View style={MyFlightsStyles.centerHorizontal}>
         <CrossPlatformIcon
           name="add-circle"
